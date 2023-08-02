cae70fcdb10d691e5e410f1e92eabcb792068fe4
Bhavanidevi11 <bhavani.d.selva@gmail.com>
2023-08-01 23:32:24 -0400

    Adding nrew files
    Adding nrew files

--- src/main/java/com/customer/reward/CustomerRewardServiceApplication.java
+++ src/main/java/com/customer/reward/CustomerRewardServiceApplication.java
@@ -2,9 +2,7 @@ package com.customer.reward;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-//@EntityScan("com.customer.reward.entity")
 @SpringBootApplication
 public class CustomerRewardServiceApplication {
 
